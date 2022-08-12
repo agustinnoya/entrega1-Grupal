f8efad357b92620b224d85b5a86eb34fa0613020
martinanido22 <martin.anido.22@gmail.com>
2022-08-12 16:45:59 -0300

    comentarios

--- js/validacion.js
+++ js/validacion.js
@@ -8,6 +8,10 @@ function showAlertError() {
     setTimeout('document.location.reload()',3000);
 }
 
+/*Cuando se cierra la alerta de error/éxito, al llamarlo denuevo daba error, 
+para solucionar esto, añadí setTimeout('document.location.reload()',3000)
+para recargar la página después de que se deplegara la alerta.*/
+
 document.getElementById("regBtn").addEventListener("click", () => {
 
     let pw1 = document.getElementById("password1").value;
