7b73150393a8b71079d9fe95193b478be91008ac
agustin noya <108200137+Agustinn57@users.noreply.github.com>
2022-08-12 09:12:26 -0300

    script

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,21 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+
+let contraseña = document.getElementById("password1")
+let contraseñados = document.getElementById("password2")
+
+function todocorrecto() {
+    if ((contraseña == contraseñados)) {
+        return showAlertSuccess()
+    } else {
+        showAlertError()
+    }
+
+    function showAlertSuccess() {
+        document.getElementById("alert-success").classList.add("show");
+    }
+    
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
+
+}
 }
