bc1fc7a69bbfbea5cf5d12d1b950a836bf43abdb
martinanido22 <martin.anido.22@gmail.com>
2022-08-12 09:09:50 -0300

    Cambios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function checkPassword() {
+    let pass1 = document.getElementById("password1");
+    let pass2 = document.getElementById("password2");
+    if (pass1 == pass2) {
+        return showAlertSuccess();
+    } else {
+        return showAlertError();
+    }
+}
