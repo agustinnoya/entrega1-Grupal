08b649d17518c02655378449a57c9dd1b0166b05
agustin noya <108200137+Agustinn57@users.noreply.github.com>
2022-08-12 10:02:13 -0300

    viernes

--- js/validacion.js
+++ js/validacion.js
@@ -1,21 +1,27 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
 
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
 
-let contraseña = document.getElementById("password1")
-let contraseñados = document.getElementById("password2")
+}
+console.log(2)
+document.getElementById("regBtn").addEventListener("click", () => {
 
-function todocorrecto() {
-    if ((contraseña == contraseñados)) {
-        return showAlertSuccess()
+console.log(3)
+    let nombre = document.getElementById("nombre").value
+    let apellido = document.getElementById("apellido").value
+    let email = document.getElementById("email").value
+    let terminos = docuemnt.getElementById("terminos").checked
+    let contraseña = document.getElementById("password1").value
+    let contraseñados = document.getElementById("password2").value
+
+    if ((contraseña.length > 5 && contraseña == contraseñados && nombre > 0 && apellido > 0 && email > 0 && terminos.checked == true)) {
+         showAlertSuccess()
     } else {
-        showAlertError()
+         showAlertError()
     }
+    console.log(4)
+})
 
-    function showAlertSuccess() {
-        document.getElementById("alert-success").classList.add("show");
-    }
-    
-    function showAlertError() {
-        document.getElementById("alert-danger").classList.add("show");
-
-}
-}
